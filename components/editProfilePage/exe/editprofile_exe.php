58f9595ffa66b775af1c9b79fee87820b44aa2e2
Gbex204 <gabriel.mocellin204@gmail.com>
2022-10-22 17:42:30 -0300

    Updates 17:42

--- components/editProfilePage/exe/editprofile_exe.php
+++ components/editProfilePage/exe/editprofile_exe.php
@@ -1,3 +1,12 @@
 <?php
+    session_start();
+    $email = $_SESSION['email'];
 
+//    if( isset($_POST['update']) ){
+        $file = addslashes(file_get_contents($_FILES['change_image_input']['tmp_name']));
+        $sql = "UPDATE cadastros SET imagem = '$file' WHERE email = '$email'";
+//    }
+//    else{
+        echo"ERRO GIGANTESCO";
+//    }
 ?>
