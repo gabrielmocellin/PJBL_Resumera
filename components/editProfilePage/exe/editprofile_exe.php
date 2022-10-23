8e25d3ecc807077d854c80beb8d17586267bdcc2
Gbex204 <gabriel.mocellin204@gmail.com>
2022-10-23 18:40:22 -0300

    Updates 18:38 Edição de Perfil melhorada + menores updates

--- components/editProfilePage/exe/editprofile_exe.php
+++ components/editProfilePage/exe/editprofile_exe.php
@@ -9,7 +9,7 @@
 
     /*Caso o usuário tenha clicado no botão "update" no editprofile.php, será enviado para essa página o ($_POST['update'])
     essa verificação apenas serve para ver se o usuário realmente veio da edição de perfil.*/
-    if(isset($_POST['update'])){
+    if(file_exists($_FILES['change_image_input']['tmp_name'])){
 
         /*Confesso que eu não sei muito bem como funciona esse comando, mas quando da um input de imagem é criado
         pelo php o $_FILES com o nome do input usado*/
@@ -28,9 +28,34 @@
         $_SESSION['imagem'] = $row['imagem'];
     
     }
-    elseif (isset($_POST[''])) {
-        # code...
+
+    /*Caso o usuário tenha enviado algum nome para ser trocado por meio do ($_POST['change_name_input'])
+    essa verificação apenas serve para ver se o usuário realmente veio da edição de perfil.*/
+    if( isset($_POST['change_name_input']) && ($_POST['change_name_input'] != '') ){
+
+        $nome = $_POST['change_name_input'];
+
+        /* Atualização da nova nome no banco de dados da conta logada */
+        $sql = "UPDATE cadastros SET nome = '$nome' WHERE email = '$email'";
+        $result = $conn->query($sql);
+
+        // Puxando o novo valor do nome relacionada à pessoa logada
+        $_SESSION['nome'] = $nome;
     }
+
+    if( isset($_POST['change_bio_input']) && ($_POST['change_bio_input'] != '') ){
+
+        $bio = $_POST['change_bio_input'];
+
+        /* Atualização da nova nome no banco de dados da conta logada */
+        $sql = "UPDATE cadastros SET bio = '$bio' WHERE email = '$email'";
+        $result = $conn->query($sql);
+
+        // Puxando o novo valor do nome relacionada à pessoa logada
+        $_SESSION['bio'] = $bio;
+    }
+
+
     else{
         echo"ERRO GIGANTESCO";
         header('location: ../../loginPage/login.php');
